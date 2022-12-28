23189514d06844c61af0fe7df90f450280bd592c
Elsen03 <114475327+elshan313@users.noreply.github.com>
2022-12-28 13:03:51 +0400

    Add files via upload

---
+++ rpss/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpVzxA1HX4oOpEjhwm5rPYT35OxnCNgaY",
+  authDomain: "rockpaperscissors-f3d80.firebaseapp.com",
+  projectId: "rockpaperscissors-f3d80",
+  storageBucket: "rockpaperscissors-f3d80.appspot.com",
+  messagingSenderId: "826709556860",
+  appId: "1:826709556860:web:e12dfd9a3f88a8d8a853f9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
